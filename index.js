e77f7aad963fa7d2791293f9874e320f38a64e36
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-01-19 19:14:10 +0000

    Employee.js file completed, constructor function made and exported

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
 // Create Employee, Engineer, Intern and Manager classes so that it'll pass the test
