592439e333e8a432b8f1c0ac3167626433c7beb2
Matt Lightfoot <m-lightfoot@hotmail.com>
2024-01-19 18:21:20 +0000

    Added jest and inquirer

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// Create Employee, Engineer, Intern and Manager classes so that it'll pass the test
+// Create the inquirer prompt to ask for Manager Information
+// Create the inquirer flow so that it will ask different set of inquirer prompts based on user choice
+// Write html result to file 
